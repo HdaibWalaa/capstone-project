73f17f9d89e2b8bdb7f592f82a6cf346eaf0938b
HdaibWalaa <105272363+HdaibWalaa@users.noreply.github.com>
2023-01-31 18:53:53 +0300

    Update index.php

--- pos/resources/views/sales/index.php
+++ pos/resources/views/sales/index.php
@@ -1,67 +1,66 @@
-<div class="row d-flex box p-1 transaction-container">
+    <div class="row d-flex box p-1 transaction-container">
 
 
-    <div class="d-flex justify-content-end mb-4 item_title pt-0 ">
-        <strong>Total Sales:&nbsp;</strong>
-        <span id="total-sales">0 </span>
+        <div class="d-flex justify-content-end mb-4 item_title pt-0 ">
+            <strong>Total Sales:&nbsp;</strong>
+            <span id="total-sales">0 </span>
+        </div>
+
+
+        <div class="d-flex flex-wrap" style="width:60%">
+            <?php foreach ($data->items as $item) {
+                if ($item->quantity > 0) { ?>
+                    <div class="card m-3" style="width:150px">
+                        <?php if (isset($item->image) && !empty($item->image)) : ?>
+                            <img id="card_single" src="./resources/image/<?= $item->image ?>" alt="" width='100px' height='90px' class="mt-3 m-auto">
+                        <?php endif; ?>
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $item->item_name ?></h5>
+                            <form class="d-flex">
+                                <input type="hidden" name="user_id" value="<?= $data->id ?>">
+                                <input type="hidden" name="items_id" value="<?= $item->id ?>">
+                                <input type="number" class="form-control w-70" placeholder="quan" aria-describedby="addon-wrapping" min="0" name="quantity" required>
+                                <button class="btn btn-outline-info ml-2">Add</button>
+                            </form>
+                        </div>
+                    </div>
+            <?php }
+            } ?>
+        </div>
+        <?php if (!empty($_SESSION) && isset($_SESSION['error']) && !empty($_SESSION['error'])) : ?>
+
+            <div class='alert alert-danger' role='alert'>
+                <?= $_SESSION['error'] ?>
+            </div>
+        <?php
+
+            $_SESSION['error'] = null;
+
+        endif; ?>
+        <div style="width:40%">
+            <div id="dataTableContainer">
+                <h2 class="text-center m-auto m-5">Transactions</h2>
+                <hr>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col" class="text-center tranaction-id">transaction id</th>
+                            <th scope="col" class="text-center item-id"> item id</th>
+                            <th scope="col" class="text-center"> item Name</th>
+                            <th scope="col" class="text-center"> quantity</th>
+                            <th scope="col" class="text-center unit-price"> Unit price</th>
+                            <th scope="col" class="text-center">total</th>
+                        </tr>
+                    </thead>
+                    <tbody id="transaction">
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+
+
+
     </div>
-
-
-    <form id="submit_form" class="my-4 d-flex justify-content-between input-form">
-        <input type="hidden" id="user_id" name="user_id" value="<?= $data->id ?>">
-        <div class="input-group flex-nowrap me-2">
-            <select id="items_id" class="form-select" aria-label="Default select example" name="items_id" required>
-                <option selected>Select One Of The Items</option>
-                <?php foreach ($data->items as $item) {
-                    $name = array();
-                    if (!in_array($item->item_name, $name) && $item->quantity > 0) {
-                        $name[] = $item->item_name; ?>
-                        <option value="<?= $item->id ?>"><?= $item->item_name ?></option>
-                    <?php  } ?>
-                <?php } ?>
-            </select>
-        </div>
-
-        <div class="input-group flex-nowrap quantitiy-space">
-            <input id="quantity" type="number" class="form-control" placeholder="Enter the quantity" aria-describedby="addon-wrapping" min="0" name="quantity" required>
-
-            <button id="add-item" class="btn py-2 pl-0 border-left btn-outline-info">Add</button>
-        </div>
-
-
-
-    </form>
-    <?php if (!empty($_SESSION) && isset($_SESSION['error']) && !empty($_SESSION['error'])) : ?>
-
-        <div class='alert alert-danger' role='alert'>
-            <?= $_SESSION['error'] ?>
-        </div>
-    <?php
-
-        $_SESSION['error'] = null;
-
-    endif; ?>
-
-    <div id="dataTableContainer">
-        <h2 class="text-center m-auto m-5">Transactions</h2>
-        <hr>
-
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col" class="text-center tranaction-id">transaction id</th>
-                    <th scope="col" class="text-center item-id"> item id</th>
-                    <th scope="col" class="text-center"> item Name</th>
-                    <th scope="col" class="text-center"> quantity</th>
-                    <th scope="col" class="text-center unit-price"> item price</th>
-                    <th scope="col" class="text-center">total</th>
-                    <th scope="col" class="text-center">Action</th>
-                </tr>
-            </thead>
-            <tbody id="transaction">
-
-            </tbody>
-        </table>
-
     </div>
-</div>
